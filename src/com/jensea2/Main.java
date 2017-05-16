ca23cc8d8e6856e3fd74508a309c8abb93a2398b
jensea2 <staypuppy@gmail.com>
2017-05-16 10:15:26 -0600

    Added user input to select number of rolls to take

--- src/com/jensea2/Main.java
+++ src/com/jensea2/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 
         rollDice.rollBoth();
-       // ScannerImport.rollTaking();
+
 
     }
 }
--- src/com/jensea2/rollDice.java
+++ src/com/jensea2/rollDice.java
@@ -1,14 +1,19 @@
 package com.jensea2;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class rollDice {
 
     public static void rollBoth() {
 
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("How many roles do you want to take: ");
+        int rolls = userInput.nextInt();
+
         Random dice = new Random();
 
-        for (int rollNumber = 1; rollNumber < 11; rollNumber++) {
+        for (int rollNumber = 1; rollNumber <= rolls; rollNumber++) {
 
             int rollDice1 = dice.nextInt(6) + 1;
             int rollDice2 = dice.nextInt(6) + 1;
