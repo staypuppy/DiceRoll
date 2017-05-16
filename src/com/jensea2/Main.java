23599b0d00a50a18b4dfc6b36a2911b38cfc5ff5
jensea2 <staypuppy@gmail.com>
2017-05-16 10:01:25 -0600

    Optimized

--- src/com/jensea2/Main.java
+++ src/com/jensea2/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
 
-        rollDiceAgain.rollBoth();
-        ScannerImport.rollTaking();
+        rollDice.rollBoth();
+       // ScannerImport.rollTaking();
 
     }
 }
--- src/com/jensea2/rollDice.java
+++ src/com/jensea2/rollDice.java
@@ -2,19 +2,17 @@ package com.jensea2;
 
 import java.util.Random;
 
+public class rollDice {
 
-public class rollDiceAgain {
-
-    //roll 2 dice and display their random values
     public static void rollBoth() {
 
-        Random dice1 = new Random();
-        Random dice2 = new Random();
+        Random dice = new Random();
 
         for (int rollNumber = 1; rollNumber < 11; rollNumber++) {
 
-            int rollDice1 = dice1.nextInt(6) + 1;
-            int rollDice2 = dice2.nextInt(6) + 1;
+            int rollDice1 = dice.nextInt(6) + 1;
+            int rollDice2 = dice.nextInt(6) + 1;
+
             System.out.println("Roll " + rollNumber + ":");
             System.out.println("Dice One: " + rollDice1);
             System.out.println("Dice Two: " + rollDice2);
