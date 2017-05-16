439798d9978c4cede67934c73e823c919de68afc
jensea2 <6Zecima8!>
2017-05-15 18:00:05 -0600

    Solved for Problem 1 - Added summation by setting random in temp variables and summing variables.

--- src/com/jensea2/rollDiceAgain.java
+++ src/com/jensea2/rollDiceAgain.java
@@ -5,18 +5,21 @@ import java.util.Random;
 public class rollDiceAgain {
 
     //roll 2 dice and display their random values
-    public static int rollBoth() {
+    public static void rollBoth() {
 
         Random dice1 = new Random();
         Random dice2 = new Random();
 
         for (int rollNumber = 1; rollNumber < 11; rollNumber++) {
+
+            int rollDice1 = dice1.nextInt(6) + 1;
+            int rollDice2 = dice2.nextInt(6) + 1;
             System.out.println("Roll " + rollNumber + ":");
-            System.out.println("Dice One: " + (dice1.nextInt(6) + 1));
-            System.out.println("Dice Two: " + (dice2.nextInt(6) + 1));
-            Random sum = dice1 + dice2;
+            System.out.println("Dice One: " + rollDice1);
+            System.out.println("Dice Two: " + rollDice2);
+            int sum = rollDice1 + rollDice2;
+            System.out.println("Sum: " + sum);
             System.out.println();
         }
-        return 0;
-    }
+          }
 }
